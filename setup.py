836bfbcf4e393ff4a1990ccf45dba54ee47a3092
PatilSri585 <sri.patil321@gmail.com>
2026-03-25 12:44:44 +0530

    Project structre built using cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project covers end to end aspects of an MLOPS Project',
+    author='Valmoori Sri Harsha',
+    license='MIT',
+)
